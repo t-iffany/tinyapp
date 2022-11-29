7826d297fcb6f795594349c9d84bde16c4f2860f
t-iffany <tiffanyxleong@gmail.com>
2022-11-29 02:44:41 +0000

    Implement function to generate string of 6 random alphanumeric characters

--- express_server.js
+++ express_server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+// Implement function that returns a string of 6 random alphanumeric characters
+function generateRandomString () {
+  return Math.random().toString(36).substr(2, 6);
+};
+
 // set ejs as the view engine
 app.set('view engine', 'ejs');
 
