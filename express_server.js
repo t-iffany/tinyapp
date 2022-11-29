8f4fbfb84bdb6562f71248c543fe68de9d77bbf6
t-iffany <tiffanyxleong@gmail.com>
2022-11-29 02:11:26 +0000

    Add GET route to show the form in the browser

--- express_server.js
+++ express_server.js
@@ -16,7 +16,12 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
-//second route for url shortened form
+// route to render the urls_new.ejs template to present form to the user
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+// route for url shortened form
 app.get('/urls/:id', (req, res) => {
   const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render('urls_show', templateVars);
