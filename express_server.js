38dd32f52e159c279d183bc52aaaf21415619fc9
t-iffany <tiffanyxleong@gmail.com>
2022-11-29 01:13:54 +0000

    Add a second route and template to create another page to display single URL and its shortened form

--- express_server.js
+++ express_server.js
@@ -16,6 +16,12 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+//second route for url shortened form
+app.get('/urls/:id', (req, res) => {
+  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render('urls_show', templateVars);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
