3845b5861b058b6d63adea11705d850facdeb9f1
t-iffany <tiffanyxleong@gmail.com>
2022-11-30 22:18:25 +0000

    Add /logout endpoint in server to clear username cookie using POST and redirect user back to /urls

--- express_server.js
+++ express_server.js
@@ -108,6 +108,12 @@ app.post("/login", (req, res) => {
   res.redirect("/urls");
 });
 
+// endpoint to handle a POST to /logout
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
