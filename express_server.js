c35b73dae82fe9929239e5ce0fafb060530e921f
t-iffany <tiffanyxleong@gmail.com>
2022-12-01 01:48:10 +0000

    Update the Login handler to use email and password fields and sets user_id cookie on successful login. Set errors conditions to POST /login endpoint.

--- express_server.js
+++ express_server.js
@@ -9,22 +9,22 @@ app.set('view engine', 'ejs');
 // takes form data and putting it into req.body
 // extended = false uses standard values ; extended = true uses non-standard values
 // body-parser library converts the request body from a Buffer into a readable string
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // cookie-parser serves as Express middleware that helps us read the values from the cookie
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
 // Implement function that returns a string of 6 random alphanumeric characters
-function generateRandomString () {
+function generateRandomString() {
   return Math.random().toString(36).substr(2, 6);
 };
 
 // Implement a function to check if email already exists. It takes an email as a parameter and return either the entire user object or null if not found
-function ifUserExists (email) {
+function ifEmailExists(email) {
   for (const user in users) {
     if (email === users[user].email) {
-      return true;
+      return users[user].id;
     };
   };
   return false;
@@ -64,7 +64,7 @@ app.get("/urls/new", (req, res) => {
 // route for url shortened form 
 app.get('/urls/:id', (req, res) => {
   const templateVars = {
-    id: req.params.id, 
+    id: req.params.id,
     longURL: urlDatabase[req.params.id],
     user: users[req.cookies['user_id']]
   };
@@ -75,7 +75,7 @@ app.get('/urls/:id', (req, res) => {
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
-}); 
+});
 
 // homepage
 app.get("/", (req, res) => {
@@ -102,7 +102,7 @@ app.post("/urls/:id", (req, res) => {
 // DELETE
 // POST route that deletes/removes a URL resource
 app.post("/urls/:id/delete", (req, res) => {
-  const idToDelete = req.params.id
+  const idToDelete = req.params.id;
   delete urlDatabase[idToDelete];
   res.redirect('/urls');
 });
@@ -139,7 +139,7 @@ app.post("/register", (req, res) => {
     res.send(400, "Please enter a valid email and password");
   };
 
-  if (ifUserExists(enteredEmail)) {
+  if (ifEmailExists(enteredEmail)) {
     res.send(400, "Account with this email already exists");
   };
 
@@ -151,20 +151,34 @@ app.post("/register", (req, res) => {
   };
 
   res.cookie("user_id", newUserID);
-  console.log('object data:', users[newUserID]);
   res.redirect("/urls");
 });
 
 // GET /login endpoint which returns login template
 app.get("/login", (req, res) => {
   res.render('urls_login');
-})
+});
 
 // endpoint to handle a POST to /login
 app.post("/login", (req, res) => {
-  const user = users[req.body['user_id']];
-  res.cookie("user", user_id);
-  res.redirect("/urls");
+  const enteredEmail = req.body.email;
+  const enteredPassword = req.body.password;
+  // error if email is not currently registered
+  if (!ifEmailExists(enteredEmail)) {
+    res.status(403).send("User with that email cannot be found");
+
+  } else {
+    // error if enteredPassword is not same as password registered to that userID
+    const existingUserID = ifEmailExists(enteredEmail);
+    if (enteredPassword !== users[existingUserID].password) {
+      res.status(403).send("Incorrect password");
+
+    } else {
+      // if email exists and passwords match, set cookie to the user id
+      res.cookie("user_id", existingUserID);
+      res.redirect("/urls");
+    }
+  }
 });
 
 // endpoint to handle a POST to /logout
